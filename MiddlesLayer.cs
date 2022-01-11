4c6dfbd38b110b9f9c338645c61e2873664016f6
mtzinkil <76640563+mtzinkil@users.noreply.github.com>
2022-01-11 08:24:39 -0500

    Add files via upload

---
+++ MiddlesLayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DataLayer;
+
+namespace MiddleServer
+{
+    public class MiddleLayer
+    {
+        /* create a dynamic array for each different data entity */
+        private List<Product> allProducts = new List<Product>();
+        private List<Salesperson> allSalespeople = new List<Salesperson>();
+        private List<Customer> allCustomers = new List<Customer>();
+        private List<Sales> allSales = new List<Sales>();
+        private List<Discount> allDiscount = new List<Discount>();
+            
+    }
+
+}/* method create a new product */
